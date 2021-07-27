927bf7f12ff2ee1441760fd357328b16f74d4335
Kalan Prudhomme <cleaver9@gmail.com>
2021-07-27 08:30:36 -0700

    working application

--- src/components/color-randomizer/ColorRandomizer.jsx
+++ src/components/color-randomizer/ColorRandomizer.jsx
@@ -3,26 +3,23 @@ import React, { Component } from 'react';
 import ColorDisplay from '../color-picker/ColorDisplay';
 
 
-const colors = ['red', 'orange', 'yellow', 'blue', 'violet', 'green'];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-const timer() {
-  
-}
 class ColorRandomizer extends Component {
 
 state = {
-  time: '',
-  color: randomColor,
+  color: '',
 }
+  
+colorPicker = () => {
+  const colors = ['red', 'orange', 'yellow', 'blue', 'violet', 'green'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  this.setState({ color: randomColor });
+};
 
   componentDidMount = () => {
-    this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+    this.interval = setInterval(() => ({ color: this.colorPicker() }), 1000);
   };
-  componentWillUnmount = () => {
-    clearInterval(this.interval());
-  };
-
+ 
   render() {
     const { color } = this.state;
     return (
